05d2e52ffe306dc98935e3b22d89031c52bbd280
Vanjula <vanjulasankar@gmail.com>
2024-08-16 21:42:50 +0530

    login img

--- vishwas/src/pages/login/Login.tsx
+++ vishwas/src/pages/login/Login.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './login.css'; 
+import img from '../../assets/cereals.jpg';
 
 
 interface LoginFormValues {
