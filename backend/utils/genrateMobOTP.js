a2d243a5d035da3a1e4fb6154090e0593b16fee0
Vijay Bharath V <vijaybharathslmfencing@gmail.com>
2024-08-21 15:10:56 +0530

    Twilio Mobile OTP
    SSID, Auth Token and mobile number added

--- backend/utils/genrateMobOTP.js
+++ backend/utils/genrateMobOTP.js
@@ -1,13 +1,26 @@
 import dotenv from 'dotenv';
 
-dotenv.config();
+require(dotenv.config());
 
 const verificationStore = new Map();
 const verifiedNumber = new Map();
 
+const twilio = require('twilio');
+
+const client = twilio(process.env.AC7f3651cc73691658de1aa966dd6cba29, process.env.0f111bfedc1f229077012b5486ee23d6);
+
+
 const generateVerificationCode = () => {
+    client.messages.create({
+        body: 'Hello from Twilio!',
+        from: process.env.+19542895323,
+        to: '+1234567890'  
+    })
+    .then(message => console.log(`Message sent with SID: ${message.sid}`))
+    .catch(error => console.error('Error sending message:', error));
     return Math.floor(100000 + Math.random() * 900000);
 };
+
 
 
 export {verificationStore,generateVerificationCode,verifiedNumber};
