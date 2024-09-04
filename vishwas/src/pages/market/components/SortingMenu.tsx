f6cc8bc998b39ac5b5c1d76dcd5df9570b80587f
Varshini <varshinis.22cse@kongu.edu>
2024-09-04 23:34:08 +0530

    market

--- vishwas/src/pages/market/components/SortingMenu.tsx
+++ vishwas/src/pages/market/components/SortingMenu.tsx
@@ -26,13 +26,7 @@ const SortingMenu = ({ onSort }: SortingMenuProps) => {
           Least Popular
         </button>
         
-        <h3 className="text-gray-600">Category-Based Sorting</h3>
-        <button 
-          onClick={() => onSort('category')}
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        >
-          Category
-        </button>
+        
       </div>
     </div>
   );
